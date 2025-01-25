915364a19141905ed061bb7a4859cb47c0b2fca6
Albaraazain <albaraazain33@gmail.com>
2025-01-25 10:21:24 +0300

    Refactor RescheduleRequestsScreen constructor syntax

--- lib/features/homeowner/screens/reschedule_requests_screen.dart
+++ lib/features/homeowner/screens/reschedule_requests_screen.dart
@@ -4,7 +4,7 @@ import '../../../providers/schedule_provider.dart';
 import 'package:provider/provider.dart';
 
 class RescheduleRequestsScreen extends StatelessWidget {
-  const RescheduleRequestsScreen({Key? key}) : super(key: key);
+  const RescheduleRequestsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
