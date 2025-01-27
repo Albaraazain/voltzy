b47776f97810627899a7b316a33cf5a2d097dd80
Albaraazain <albaraazain33@gmail.com>
2025-01-27 22:18:49 +0300

    Refactor ServiceCategoryCard to be a StatelessWidget with dynamic image and styling

--- lib/features/homeowner/models/service_category_card.dart
+++ lib/features/homeowner/models/service_category_card.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 
-enum CardSize {
-  small, // 1x1
-  medium, // 2x1
-  large // 2x2
-}
+enum CardSize { small, medium, large }
 
-class ServiceCategoryCard {
+class ServiceCategoryCard extends StatelessWidget {
   final String id;
   final String name;
   final String description;
@@ -15,9 +11,10 @@ class ServiceCategoryCard {
   final double minPrice;
   final double maxPrice;
   final CardSize size;
-  final Color accentColor;
+  final MaterialColor accentColor;
 
   const ServiceCategoryCard({
+    super.key,
     required this.id,
     required this.name,
     required this.description,
@@ -29,24 +26,133 @@ class ServiceCategoryCard {
     required this.accentColor,
   });
 
-  // Helper method to get the grid item span based on card size
-  int get columnSpan {
-    switch (size) {
-      case CardSize.small:
-        return 1;
-      case CardSize.medium:
-      case CardSize.large:
-        return 2;
+  @override
+  Widget build(BuildContext context) {
+    String imagePath = 'assets/images/';
+    switch (name.toLowerCase().replaceAll(' services', '')) {
+      case 'electrical':
+        imagePath += 'electrical_services.png';
+        break;
+      case 'plumbing':
+        imagePath += 'plumbing_services.png';
+        break;
+      case 'hvac':
+        imagePath += 'hvac_services.png';
+        break;
+      case 'landscaping':
+        imagePath += 'landscaping_services.png';
+        break;
+      case 'security systems':
+      case 'security':
+        imagePath += 'security_services.png';
+        break;
+      case 'smart home':
+        imagePath += 'smart_home_services.png';
+        break;
+      case 'home cleaning':
+      case 'cleaning':
+        imagePath += 'home_cleaning_services.png';
+        break;
+      case 'carpentry':
+        imagePath += 'carpentry_services.png';
+        break;
+      case 'painting':
+        imagePath += 'painting_services.png';
+        break;
+      case 'solar services':
+      case 'solar':
+        imagePath += 'solar_services.png';
+        break;
+      case 'appliance repair':
+      case 'appliance':
+        imagePath += 'appliance_repair_services.png';
+        break;
+      default:
+        imagePath += 'default_service.png';
     }
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(32),
+        image: DecorationImage(
+          image: AssetImage(imagePath),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(32),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.transparent,
+              Colors.black.withOpacity(0.6),
+            ],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.4),
+                borderRadius: BorderRadius.circular(8),
+                border: Border.all(
+                  color: Colors.white.withOpacity(0.2),
+                  width: 0.5,
+                ),
+              ),
+              child: Text(
+                name.replaceAll(' Services', ''),
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                  letterSpacing: 0.3,
+                ),
+              ),
+            ),
+            const Spacer(),
+            Text(
+              '$serviceCount services',
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.white70,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              '\$${minPrice.toStringAsFixed(0)}-${maxPrice.toStringAsFixed(0)}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
-  int get rowSpan {
-    switch (size) {
-      case CardSize.small:
-      case CardSize.medium:
-        return 1;
-      case CardSize.large:
-        return 2;
+  IconData _getIconData() {
+    switch (iconName.toLowerCase()) {
+      case 'cleaning':
+        return Icons.cleaning_services;
+      case 'maintenance':
+        return Icons.build;
+      case 'installation':
+        return Icons.settings;
+      case 'repair':
+        return Icons.handyman;
+      case 'landscaping':
+        return Icons.grass;
+      default:
+        return Icons.home_repair_service;
     }
   }
 }
